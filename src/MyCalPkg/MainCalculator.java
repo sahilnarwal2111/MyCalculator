ecfad0bbd8f20855e621731e0245b4a514405afa
sahilnarwal2111 <500097751@stu.upes.ac.in>
2022-10-17 13:39:47 +0530

    updated main method

---
+++ src/MyCalPkg/MainCalculator.java
@@ -0,0 +1,12 @@
+package MyCalPkg;
+
+public class MainCalculator {
+	public static void main(String [] argss) {
+		Calculator c = new Calculator();
+		System.out.println(c.addition(100,5));
+		System.out.println(c.addition(100,5));
+		System.out.println(c.addition(100,5));
+		System.out.println(c.addition(100,5));
+	}
+
+}
